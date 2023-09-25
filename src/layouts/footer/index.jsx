ed5c41dc089ed9d5ac59dd042c165f560cd7aff4
grbzhmet <grbzhmet@gmail.com>
2023-09-25 22:18:49 +0300

    footer add

---
+++ src/layouts/footer/index.jsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+  return(
+    <footer className="w-full h-[72px] bg-black fixed bottom-0" />
+    )
+}
--- src/layouts/main/index.jsx
+++ src/layouts/main/index.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "../sidebar";
+import Footer from "../footer";
 
 export default function MainLayout() {
   return (
-    <div className="w-100 mx-auto flex">
+    <div className="">
+      <div className="w-100 mx-auto flex">
       <SideBar />
       <main className="flex-1 px-2 py-3 ml-2 bg-neutral-900 rounded-lg min-h-screen">
         <Outlet />
       </main>
-      
+      </div>
+      <Footer />
     </div>
   )
 }
