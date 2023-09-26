302d5875b249055920b88e937985ff01830592a0
grbzhmet <grbzhmet@gmail.com>
2023-09-26 21:35:24 +0300

    footer yapısı oluşturuldu

--- src/layouts/footer/index.jsx
+++ src/layouts/footer/index.jsx
@@ -1,5 +1,9 @@
 export default function Footer() {
   return(
-    <footer className="w-full h-[72px] bg-black fixed bottom-0" />
+    <footer className="w-full h-[72px] bg-black fixed bottom-0 flex flex-row place-items-center">
+        <div className="basis-1/4 flex items-center justify-center">şarkı alanı</div>
+        <div className="basis-1/2 flex items-center justify-center">müzik çalar</div>
+        <div className="basis-1/4 flex items-center justify-center">ses ayarları</div>
+    </footer>
     )
 }
